f168e3d224e4be686433e04ff4dcb183ba54851b
italodesa <italodesa0101@gmail.com>
2026-04-06 15:23:04 -0300

    refactor: changing attribute id to product_id

--- src/products.py
+++ src/products.py
@@ -1,6 +1,6 @@
 class Product:
     def __init__(self,name,price,quantity):
-        self.id = None
+        self.product_id = None
         self.name = name
         self.price = price
         self.quantity = quantity
