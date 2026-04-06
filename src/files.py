8abaac912620d4eea2fdabe8fb36411b5eb40a54
italodesa <italodesa0101@gmail.com>
2026-04-06 15:07:05 -0300

    feat: add write_file function

--- src/files.py
+++ src/files.py
@@ -19,3 +19,10 @@ def return_data(path):
     with open(path,"r",encoding="utf-8") as f:
         data = json.load(f)
     return data
+
+def write_file(path,your_data):
+    path = verify(path)
+    path_data = return_data(path)
+    path_data.append(your_data)
+    with open(path,"w",encoding="utf-8") as f:
+        json.dump(path_data, f, indent=4, ensure_ascii=False)
