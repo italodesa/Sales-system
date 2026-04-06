0f057585512c4671b7b1c68ee580b44899e695ab
italodesa <italodesa0101@gmail.com>
2026-04-06 10:46:33 -0300

    feat: add  create_product class  method

--- src/products.py
+++ src/products.py
@@ -4,3 +4,10 @@ class Product:
         self.name = name
         self.price = price
         self.quantity = quantity
+    
+    @classmethod
+    def create_product(cls):
+        name = input("Digite o nome do produto: ")
+        price = float(input("Digite o preço do produto: "))
+        quantity = int(input("Digite a quantidade do produto: "))
+        return cls(name,price,quantity)
