500e10b45d23bab9fcf23838299ec4df016b373a
italodesa <italodesa0101@gmail.com>
2026-04-07 09:23:28 -0300

    feat: add login module and login_menu function

---
+++ src/login.py
@@ -0,0 +1,18 @@
+from files import return_data
+def login_menu():
+    users = return_data("users.json")
+    print("#" * 50)
+    print(" " * 20 + "Login" + " " * 20)
+    print("#" * 50)
+    name = input("Digite seu nome: ")
+    password = input("Digite sua senha: ")
+
+    if users:
+        for user in users:
+            if (name == user["name"]) and (password == user["password"]):
+                return user
+        print("Usuario não cadastrado")
+        return None
+    else:
+        print("Não existe nenhum usuario cadastrado")
+        return None
