4c1c5547d39f91478e2b194185a90f5bcc996f7c
italodesa <italodesa0101@gmail.com>
2026-04-07 09:40:40 -0300

    feat: add Seller class

---
+++ src/seller.py
@@ -0,0 +1,6 @@
+class Seller:
+    def __init__(self,name,password,seller_id=None):
+        self.name = name
+        self.password = password
+        self.seller_id = seller_id
+        self.profile = "seller"
