1d232abb75e89b4660297c75b29df3a503b2474d
italodesa <italodesa0101@gmail.com>
2026-04-07 09:58:31 -0300

    feat: add create_product_admin method

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,12 @@
+from products import Product
+from files import return_data,write_file
 class Admin:
     def __init__(self,name,password,admin_id=None):
         self.name = name
         self.password = password
         self.admin_id = admin_id
         self.profile = "admin"
+    
+    def create_product_admin(self):
+        product = Product.create_product()
+        write_file('products.json',product.__dict__)
