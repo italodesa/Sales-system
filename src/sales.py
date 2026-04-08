aa3e043eeec52ec4892beb23d9bfd267e21a7bb2
italodesa <italodesa0101@gmail.com>
2026-04-08 14:35:08 -0300

    feat: add create_sale method

--- src/sales.py
+++ src/sales.py
@@ -1,7 +1,8 @@
 class Sale:
-    def __init__(self,sale_id,seller_id,seller_name,total,create_in):
+    def __init__(self,sale_id,seller_id,seller_name,total,create_in,products):
         self.sale_id = sale_id
         self.seller_id = seller_id
         self.seller_name = seller_name
         self.total = total
         self.create_in = create_in
+        self.products = products
--- src/seller.py
+++ src/seller.py
@@ -1,6 +1,36 @@
+from datetime import datetime
+from sales import Sale
+from products import Product
+
 class Seller:
     def __init__(self,name,password,seller_id=None):
         self.name = name
         self.password = password
         self.seller_id = seller_id
         self.profile = "seller"
+
+    def create_sale(self):
+        total = 0.0
+        products = []
+        while True:
+            now = datetime.now()
+            print("-" * 50)
+            print(" " * 20 + "painel de venda" + " " * 20)
+            print("-" * 50)
+            print(f"Vendedor: {self.name}")
+            print(f"Data: {now.strftime('%d/%m/%Y')}")
+            print("[1] Adicionar Produto\n[2] Finalizar compra\n[3] Sair")
+
+            try:
+                option = int(input("> "))
+            except ValueError:
+                print("Digite uma entrada valida")
+            
+            match option:
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    break
+            
