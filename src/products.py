12590d48f7801c704d2d4dfd6c8b5c1f4243542a
italodesa <italodesa0101@gmail.com>
2026-04-08 14:42:02 -0300

    feat(products): add show_products staticmethod

--- src/products.py
+++ src/products.py
@@ -1,4 +1,4 @@
-from files import generate_id
+from files import generate_id,return_data
 class Product:
     def __init__(self,name,price,quantity,product_id=None):
         self.product_id = product_id
@@ -14,3 +14,16 @@ class Product:
         quantity = int(input("Digite a quantidade do produto: "))
         product_id = generate_id('products.json','product_id')
         return cls(name,price,quantity,product_id)
+    
+    @staticmethod
+    def show_products(cls):
+        products = return_data('products.json')
+
+        print("-" * 40)
+        print(f"{'ID':<10} {'NOME':<20}")
+        print("-" * 40)
+
+        for product in products:
+            print(f"{product['product_id']:<10} {product['name']:<20}")
+
+        print("-" * 40)
