443516154b2477a514a2f2b3e0a651ce21a6f25c
italodesa <italodesa0101@gmail.com>
2026-04-08 14:15:38 -0300

    feat: add sales module and Sale class

---
+++ src/sales.py
@@ -0,0 +1,7 @@
+class Sale:
+    def __init__(self,sale_id,seller_id,seller_name,total,create_in):
+        self.sale_id = sale_id
+        self.seller_id = seller_id
+        self.seller_name = seller_name
+        self.total = total
+        self.create_in = create_in
